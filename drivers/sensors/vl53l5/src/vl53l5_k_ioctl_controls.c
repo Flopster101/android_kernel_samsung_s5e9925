ExtremeXT/android_kernel_samsung_exynos990

sensorhub/fbdev: fix warnings when compiling for canvas

drivers/sensorhub/brcm/st_vl53l5/src/vl53l5_k_ioctl_controls.c
@@ -2360,7 +2360,6 @@ int vl53l5_k_power_onoff(struct vl53l5_k_module_t *data,
     struct regulator *vdd_vreg, const char *vdd_reg_name, bool on)
 {
 	int ret = 0;
-	int voltage = 0;
 	int reg_enabled = 0;
 
 	if (vdd_reg_name) {
@@ -2374,9 +2373,8 @@ int vl53l5_k_power_onoff(struct vl53l5_k_module_t *data,
 	}
 
 	if (vdd_vreg) {
-		voltage = regulator_get_voltage(vdd_vreg);
 		reg_enabled = regulator_is_enabled(vdd_vreg);
-		vl53l5_k_log_info("vdd_reg reg_enabled=%d voltage=%d\n", reg_enabled, voltage);
+		vl53l5_k_log_info("vdd_reg reg_enabled=%d voltage=%d\n", reg_enabled, regulator_get_voltage(vdd_vreg));
 
 		if (on) {
 			if (reg_enabled == 0) {

Flopster101/android_kernel_samsung_s5e9925

Change-Id: Ifc72b6ef59b88c708441afe4073437e6dd578f65

drivers/sensors/vl53l5/src/vl53l5_k_ioctl_controls.c
@@ -2775,7 +2775,6 @@ out_state:
 int vl53l5_k_power_onoff(struct vl53l5_k_module_t *data, int io, bool on)
 {
 	int ret = 0;
-	int voltage = 0;
 	int reg_enabled = 0;
 
 	vl53l5_k_log_info("%d : %s\n", io, on ? "on" : "off");
@@ -2794,9 +2793,8 @@ int vl53l5_k_power_onoff(struct vl53l5_k_module_t *data, int io, bool on)
 		}
 
 		if (data->avdd_vreg) {
-			voltage = regulator_get_voltage(data->avdd_vreg);
 			reg_enabled = regulator_is_enabled(data->avdd_vreg);
-			vl53l5_k_log_info("avdd reg_enabled=%d voltage=%d\n", reg_enabled, voltage);
+			vl53l5_k_log_info("avdd reg_enabled=%d voltage=%d\n", reg_enabled, regulator_get_voltage(vdd_vreg));
 
 			if (on) {
 				if (reg_enabled == 0) {